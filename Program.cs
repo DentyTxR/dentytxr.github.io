c8e808ee79e409aecb88e56712a4fd9e81d8f88e
DentyTxR <dentymods.axuka@gmail.com>
2024-11-17 02:11:19 -0500

    a lot of changes, learning css more, adding new/improved loading screen, etc

--- Program.cs
+++ Program.cs
@@ -2,11 +2,16 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ghp_app;
 using MudBlazor.Services;
+using Blazorise;
+using Blazorise.Icons.FontAwesome;
+using Blazorise.Bootstrap;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
+
+builder.Services.AddBlazorise(options => {  } ).AddFontAwesomeIcons().AddBootstrapProviders();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
